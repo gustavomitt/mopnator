3c19cc84b33aab11ee58cd9e8011da58e0df5c70
gustavomitt <gustavo@actar.com.br>
2015-03-10 20:45:14 -0300

    Create gerador.py

---
+++ gerador.py
@@ -0,0 +1,20 @@
+'''
+Created on 23/02/2015
+
+@author: Owner
+'''
+
+import sys,paramiko
+
+try:
+    client = paramiko.SSHClient()
+    client.load_system_host_keys()
+    client.set_missing_host_key_policy(paramiko.WarningPolicy)
+        
+    client.connect("10.10.11.100", port=22, username="root", password="Laboratorio")
+ 
+    stdin, stdout, stderr = client.exec_command("ls -l")
+    print(stdout.read()),
+ 
+finally:
+    client.close()
